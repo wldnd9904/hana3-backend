bf4ca611d5b23206d7f51840cdbe66317701e5f1
wldnd9904 <wldnd9904@uos.ac.kr>
2024-03-29 13:59:35 +0900

    헤더에서 언어받아오기

--- springboot/Pr06VM+API/src/main/java/com/study/pr03vm/HtmlController.java
+++ springboot/Pr06VM+API/src/main/java/com/study/pr03vm/HtmlController.java
@@ -6,13 +6,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
+import javax.swing.text.html.HTML;
 import java.util.Locale;
 
 @Controller
 public class HtmlController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(@RequestHeader("Accept-Language") String acceptLanguage, HttpSession session) {
+        String LSAN = SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME;
+        if (session.getAttribute(LSAN) == null) {
+            String MainLocale = acceptLanguage.split(",")[0];
+            session.setAttribute(LSAN, Locale.forLanguageTag(MainLocale));
+        }
         return "index";
     }
 
@@ -20,6 +26,7 @@ public class HtmlController {
     public String changeLocale(HttpSession session) {
         String LSAN = SessionLocaleResolver.LOCALE_SESSION_ATTRIBUTE_NAME;
         Locale locale = (Locale) session.getAttribute(LSAN);
+        System.out.println(locale);
         if (Locale.US.equals(locale)) {
             session.setAttribute(LSAN, Locale.KOREA);
         } else {
