be465a25e439e2d4d23e367026ea247cda61e917
wldnd9904 <wldnd9904@uos.ac.kr>
2024-04-24 11:29:13 +0900

    시큐리티: 로그인

--- springboot/ex27_security_01/src/main/java/com/study/ex27_security_01/MainController.java
+++ springboot/ex27_security_01/src/main/java/com/study/ex27_security_01/MainController.java
@@ -1,2 +1,19 @@
-package com.study.ex27_security_01;public class MainController {
+package com.study.ex27_security_01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    @ResponseBody
+    public String main() {
+        return "Spring Security web desu";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm() {
+        return "loginForm";
+    }
 }
--- springboot/ex27_security_01/src/main/java/com/study/ex27_security_01/SecurityConfig.java
+++ springboot/ex27_security_01/src/main/java/com/study/ex27_security_01/SecurityConfig.java
@@ -1,2 +1,32 @@
-package com.study.ex27_security_01;public class SecurityConfig {
+package com.study.ex27_security_01;
+
+import jakarta.servlet.http.HttpServlet;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity // 웹보안 활성화 위한 어노테이션
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((auth) -> auth
+                //루트 밑의 모든 경로에 대한 모든 요청 허가
+                .requestMatchers(new AntPathRequestMatcher("/**"))
+                .permitAll()
+        ).formLogin((formLogin) -> formLogin
+                //로그인 페이지를 /loginForm URI로 하겠다
+                .loginPage("/loginForm")
+                //로그인 액션은 /loginAction URI
+                .loginProcessingUrl("/loginAction")
+                //로그인되면 여기로 넘어가라
+                .defaultSuccessUrl("/")
+                //모든 사용자에게 허용
+                .permitAll()
+        );
+        return http.build();
+    }
 }
